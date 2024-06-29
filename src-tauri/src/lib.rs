bf55639f24b4e20da8d5ef2910b3c70703a01299
AuracleTech <virgilfabre@outlook.com>
2024-06-28 21:04:06 -0400

    draven parsing

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use draven::project::parser::Parser;
+use log::debug;
+use std::path::PathBuf;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -5,10 +9,19 @@ fn greet(name: &str) -> String {
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    ::std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let mut parser = Parser::new(PathBuf::from("C:/Users/Silco/Desktop/pulsar"))?;
+    parser.parse_in_src(&PathBuf::from("lib.rs"))?;
+    debug!("{:#?}", parser);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    Ok(())
 }
